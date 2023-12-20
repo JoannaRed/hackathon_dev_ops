85df4941320f2a14265e91f7394b0f2a7e68996c
JoannaRed <j.baranowska@hotmail.com>
2023-12-20 20:16:16 +0100

    fix build

--- src/pages/newhome.data.js
+++ src/pages/newhome.data.js
@@ -2,3 +2,7 @@ export  const HandShake = {
     HANDSHAKE_REQUEST : "HANDSHAKE_REQUEST",
     HANDSHAKE_RESPONSE : "HANDSHAKE_RESPONSE"
   }
+
+  export default function Data() {
+    return <>{/* nothing */}</>;
+  }
